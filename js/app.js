74b68fa7535074ce69192e9648375d5232ed3a41
Mahamodul Hasan Moon <coddermoon@gmail.com>
2022-08-28 10:15:17 +0600

    data load done

--- js/app.js
+++ js/app.js
@@ -8,6 +8,28 @@ const loadMeals = ()=>{
 
 const displayMeals = (foods)=>{
 
+   
+
+const meals = document.getElementById('meal')
+
+    foods.forEach(food => {
+        console.log(food)
+        const {strMeal:name,strMealThumb:img}= food
+       const div = document.createElement('div');
+       div.innerHTML= `
+       <div class = "meal-item">
+       <div class = "meal-img">
+         <img src = "${img}" alt = "food">
+       </div>
+       <div class = "meal-name">
+         <h3>${name}</h3>
+         <a href = "#" class = "recipe-btn">Get Recipe</a>
+       </div>
+     </div>
+       `
+       meals.appendChild(div)
+    });
+
     
 
     
